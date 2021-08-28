ff651ae0d457cf61c25bdfe2be8cd4d32af82b3d
java-tejaswini <45378471+java-tejaswini@users.noreply.github.com>
2021-08-28 12:02:55 +0530

    Update App.java
    added

--- java-mvnproject/src/main/java/com/data/App.java
+++ java-mvnproject/src/main/java/com/data/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "WELCOME TO JENKINS" );
+        System.out.println( "WELCOME TO JENKINS **************" );
     }
 }
